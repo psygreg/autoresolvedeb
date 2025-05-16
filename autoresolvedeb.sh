e02ac199c8e1dd7df03d5ceab3c3b84b717236c4
Victor <psygreg@pm.me>
2025-05-16 14:48:37 -0300

    fixed and updated to resolve 19.1.4

--- autoresolvedeb.sh
+++ autoresolvedeb.sh
@@ -1,67 +1,30 @@
 #!/bin/bash
-#
-#
-#
-#create temp folder and set cleanup
-tmp="$(mktemp -d -t autoresolvedebXXXXX)"
-cd "$tmp" || exit 2
-#cleanup
-cleanup() {
-	rm -rf "$tmp" 
-	}
-trap cleanup EXIT
-#get language from OS
-get_lang() {
-	local lang="${LANG:0:2}"
-        local available=("pt" "en")
 
-        if [[ " ${available[*]} " == *"$lang"* ]]; then
-        	ulang="$lang"
-        else
-                ulang="en"
-        fi
-        }
-#languages - add new translations under here
-if ulang="pt"; then
-	startup () {
-		echo "Este é o AutoResolveDeb por Psygreg."
-		echo "Este programa irá baixar DaVinci Resolve dos servidores da Blackmagic Design, converter em um pacote .deb e instalá-lo."
-		echo "Qual versão deseja instalar?"
-		}
-	usrcancel="Cancelado pelo usuário."
-else
-	startup () {
-		echo "This is AutoResolveDeb by Psygreg."
-		echo "This program will download DaVinci Resolve from Blackmagic Design's source, convert it into a .deb package and install it."
-		echo "Which version do you wish to install?"
-		}
-	usrcancel="Cancelled by user."
-fi
 #create JSON, user agent and download Resolve
 getresolve() {
   	local pkgname="$_upkgname"
-  	local major_version="19.0"
-  	local minor_version="1"
-  	local pkgver="${major_version}.${minor_version}"
+  	local major_version="19.1"
+  	local minor_version="4"
+  	pkgver="${major_version}.${minor_version}"
   	local _product=""
   	local _referid=""
   	local _siteurl=""
   	local sha256sum=""
-  	local _archive_name=""
+  	_archive_name=""
   	local _archive_run_name=""
 
   	if [ "$pkgname" == "davinci-resolve" ]; then
     		_product="DaVinci Resolve"
     		_referid='dfd43085ef224766b06b579ce8a6d097'
     		_siteurl="https://www.blackmagicdesign.com/api/support/latest-stable-version/davinci-resolve/linux"
-    		sha256sum='06ba9d3e2f4e6ca813a394e0fe622992fdb6b29b9cd5f9a351103ad1040b6dac'
+    		sha256sum='40bf13b7745b420ed9add11c545545c2ba2174429b6c8eafe8fceb94aa258766'
     		_archive_name="DaVinci_Resolve_${pkgver}_Linux"
     		_archive_run_name="DaVinci_Resolve_${pkgver}_Linux"
   	elif [ "$pkgname" == "davinci-resolve-studio" ]; then
     		_product="DaVinci Resolve Studio"
     		_referid='0978e9d6e191491da9f4e6eeeb722351'
     		_siteurl="https://www.blackmagicdesign.com/api/support/latest-stable-version/davinci-resolve-studio/linux"
-    		sha256sum='27c33c942fec19533cf81fd5ebd19706e8c0fd92c6ad4da47402171b885d38e4'
+    		sha256sum='5fb4614834c5a9f990afa977b7d5dcd2675c26529bc09a468e7cd287bbaf5097'
     		_archive_name="DaVinci_Resolve_Studio_${pkgver}_Linux"
     		_archive_run_name="DaVinci_Resolve_Studio_${pkgver}_Linux"
   	fi
@@ -104,40 +67,54 @@ getresolve() {
     		"$_siteurl")
 
   	curl -L -o "${_archive_name}.zip" "$_srcurl"
-	}
-#get makeresolvedeb
+}
+
+# get makeresolvedeb
 makeresolvedeb () {
-	wget https://www.danieltufvesson.com/download/?file=makeresolvedeb/makeresolvedeb_1.7.1_multi.sh.tar.gz;
-	tar zxvf index.html?file=makeresolvedeb%2Fmakeresolvedeb_1.7.1_multi.sh.tar.gz;
-	}
-#check dependencies	
+	mrdver='1.8.1'
+	curl --output makeresolvedeb_${mrdver}_multi.sh.tar.gz https://www.danieltufvesson.com/download/?file=makeresolvedeb/makeresolvedeb_${mrdver}_multi.sh.tar.gz;
+	tar zxvf makeresolvedeb_${mrdver}_multi.sh.tar.gz;
+}
+
+# dependency check
 deps () {
-	sudo apt install -y fakeroot xorriso;
+	sudo apt install -y fakeroot xorriso libqt5gui5 curl wget;
 	}
-#installer
-installer () {
-	deps;
-	unzip "${_archive_name}.zip";
-	makeresolvedeb;
-	./makeresolvedeb_1.7.1_multi.sh "${_archive_name}.run";
-	sudo dpkg -i davinci-resolve-studio_18.6.6-mrd1.7.1_amd64.deb
-	exit 0
-	}
-#RUNTIME START
-get_lang
-startup
-select resolve_opt in "Free" "Studio" "Cancel"; do
-	case $resolve_opt in
-		Free )
-			_upkgname=davinci-resolve
-			getresolve;
-			installer;;
-		Studio )
-			_upkgname=davinci-resolve-studio
-			getresolve;
-			installer;;
-		Cancel )
-			echo "$usrcancel";
-			exit 0;;
+
+# menu
+while :; do
+	CHOICE=$(whiptail --title "AutoResolveDeb" --menu "Which version do you want to install?" 25 78 16 \
+	"0" "Free" \
+	"1" "Studio" \
+	"2" "Cancel" 3>&1 1>&2 2>&3)
+	
+	exitstatus=$?
+	if [ $exitstatus != 0 ]; then
+    	# Exit the script if the user presses Esc
+    	break
+	fi
+
+	case $CHOICE in
+	0) 	_upkgname='davinci-resolve' 
+		getresolve 
+		makeresolvedeb 
+		deps 
+		unzip ${_archive_name}.zip 
+		./makeresolvedeb_${mrdver}_multi.sh ${_archive_name}.run 
+		sudo dpkg -i davinci-resolve_${pkgver}-mrd${mrdver}_amd64.deb
+		whiptail --title "AutoResolveDeb" --msgbox "Installation succesful." 8 78 
+		exit 0 ;;
+	1) 	_upkgname='davinci-resolve-studio' 
+		getresolve 
+		makeresolvedeb 
+		deps 
+		unzip ${_archive_name}.zip 
+		./makeresolvedeb_${mrdver}_multi.sh ${_archive_name}.run 
+		sudo dpkg -i davinci-resolve-studio_${pkgver}-mrd${mrdver}_amd64.deb
+		whiptail --title "AutoResolveDeb" --msgbox "Installation succesful." 8 78 
+		exit 0 ;;
+	2 | q) break ;;
+	*) echo "Invalid Option" ;;
 	esac
 done
+
